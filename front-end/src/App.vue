1781b77f39c6fc5f0fe92c65d9e5972d13da265e
tolamson2811 <tolamson2811@gmail.com>
2023-10-03 21:32:17 +0700

    setup vue

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-green-600">Alo 12345</h1>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
+<script lang="ts">
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  created() {
+    console.log("Hello World");
+  },
+  mounted() {
+    console.log("Mounted");
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
