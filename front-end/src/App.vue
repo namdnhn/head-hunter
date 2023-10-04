2b0fb834ddb7e680bebba5ee06f649c845384bd1
tolamson2811 <tolamson2811@gmail.com>
2023-10-05 02:25:21 +0700

    folder structure

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,11 +1,12 @@
-<script setup lang="ts">
-// import {HelloWorld} from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <h1 class=" text-red-500">Hello world <font-awesome-icon icon="fa-solid fa-arrow-right" /></h1>
-  </div>
+	<the-header></the-header>
 </template>
+
+<script lang="ts">
+import TheHeader from '../src/components/layouts/TheHeader.vue';
+export default {
+	components: { TheHeader }
+}
+</script>
 
 <style scoped></style>
---
+++ front-end/src/components/layouts/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="h-20 w-full fixed bg-white flex justify-center shadow-md">
+        <div class="w-11/12 flex justify-between">
+            <nav class="flex items-center">
+                <a class="mr-16 hover:cursor-pointer">
+                    Logo here
+                </a>
+    
+                <ul class="flex gap-6">
+                    <li>
+                        <a class=" hover:text-green-700 hover:cursor-pointer">Home</a>
+                    </li>
+                    <li>
+                        <a class=" hover:text-green-700 hover:cursor-pointer">For Candidate <font-awesome-icon icon="fa-solid fa-chevron-down" /></a>
+                    </li>
+                    <li>
+                        <a class=" hover:text-green-700 hover:cursor-pointer">For Employer <font-awesome-icon icon="fa-solid fa-chevron-down" /></a>
+                    </li>
+                    <li>
+                        <a class=" hover:text-green-700 hover:cursor-pointer">Profile</a>
+                    </li>
+                    <li>
+                        <a class=" hover:text-green-700 hover:cursor-pointer">Help</a>
+                    </li>
+                </ul>
+            </nav>
+    
+            <span class="flex gap-4 items-center">
+                <a class=" hover:text-green-700 hover:cursor-pointer">Sign in</a>
+                <a class=" hover:text-green-700 hover:cursor-pointer">Sign up</a>
+            </span>
+        </div>
+    </header>
+</template>
