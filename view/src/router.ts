ff7c3764486f57f33c48ef06d1f10a5c6cd122b4
NguyenDucThien <21020407@vnu.edu.vn>
2023-10-09 21:22:55 +0700

    login page for candicate

--- view/src/router.ts
+++ view/src/router.ts
@@ -3,9 +3,9 @@ import { defineAsyncComponent } from "vue";
 
 //import page
 const HomePage = defineAsyncComponent(() => import("./pages/HomePage.vue"));
-const ProfilePage = defineAsyncComponent(
-  () => import("./pages/ProfilePage.vue")
-);
+const ProfilePage = defineAsyncComponent(() => import("./pages/ProfilePage.vue"));
+const LoginPage = defineAsyncComponent(() => import("./pages/auth/LoginPage.vue"))
+const RegisterPage = defineAsyncComponent(() => import("./pages/auth/RegisterPage.vue"))
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,6 +13,8 @@ const router = createRouter({
     { path: "/", redirect: "/homepage" },
     { path: "/homepage", component: HomePage },
     { path: "/profile", component: ProfilePage },
+    { path: "/login", component: LoginPage},
+    { path: "/register", component: RegisterPage}
   ],
 }); 
 
