1621aa32ace8604da12b10069dd040bdc7db66ee
namdnhn <donhuhoangnam.work@gmail.com>
2023-10-14 13:59:58 +0700

    feat: update connect api login

--- app/database.py
+++ app/database.py
@@ -4,18 +4,23 @@ from sqlalchemy.orm import sessionmaker
 import mysql.connector
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 try:
     mysql_connection = mysql.connector.connect(
-        user=os.getenv("DB_USERNAME"), password=os.getenv("DB_PASSWORD"), host=os.getenv("DB_HOST"), database=os.getenv("DB_NAME")
+        user=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
     )
     print("Connected to MySQL successfully!")
 except mysql.connector.Error as err:
     print(f"Error: {err}")
 
 engine = create_engine(
-    f"mysql+mysqlconnector://{ os.getenv('DB_USERNAME') }:{ os.getenv('DB_PASSWORD') }@{ os.getenv('DB_HOST') }/{ os.getenv('DB_NAME') }", echo=True
+    f"mysql+mysqlconnector://{ os.getenv('DB_USERNAME') }:{ os.getenv('DB_PASSWORD') }@{ os.getenv('DB_HOST') }/{ os.getenv('DB_NAME') }",
+    echo=True,
 )
 
 # create a SessionLocal class
@@ -23,6 +28,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 # create a Base class
 Base = declarative_base()
+
 
 def getDatabase():
     db = SessionLocal()
--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
 from fastapi import FastAPI
 from database import Base, engine
-from routes import companyRouter, userRouter
+from routes import companyRouter, userRouter, messageRouter
 from models.application import ApplicationModel
 from models.post import PostModel
 from fastapi import Depends
 from controllers.userController import reusable_oauth2, isTokenInvalidated
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(companyRouter.router, prefix="/api")
 app.include_router(userRouter.router, prefix="/api")
+app.include_router(messageRouter.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*", "sentry-trace", "baggage"],
+)
 
 @app.get("/api/ping")
 def ping(data: str = Depends(reusable_oauth2)):
