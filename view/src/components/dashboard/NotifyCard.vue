df1b308bb5e1d3f469bae79d4309f5bafc79703c
tolamson2811 <tolamson2811@gmail.com>
2023-10-26 17:16:28 +0700

    style: UserInfo page in UserDashboard

---
+++ view/src/components/dashboard/NotifyCard.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="flex w-full gap-4 items-center bg-white p-2 border-y border-slate-300">
+        <font-awesome-icon 
+            icon="fa-solid fa-reply"
+            class="p-2 bg-yellow-100 text-amber-500 rounded-full text-sm md:text-base lg:text-lg" 
+            v-if="type === 'reply'" 
+        />
+
+        <font-awesome-icon 
+            icon="fa-solid fa-thumbs-up"
+            class="p-2 bg-blue-100 text-blue-500 rounded-full text-sm md:text-base lg:text-lg" 
+            v-else-if="type === 'approve'" 
+        />
+
+        <font-awesome-icon 
+            icon="fa-solid fa-comment"
+            class="p-2 bg-green-100 text-green-600 rounded-full text-sm md:text-base lg:text-lg" 
+            v-else-if="type === 'receive'" 
+        />
+
+        <font-awesome-icon 
+            icon="fa-solid fa-ban"
+            class="p-2 bg-red-100 text-red-500 rounded-full text-sm md:text-base lg:text-lg" 
+            v-else-if="type === 'deny'" 
+        />
+
+        <font-awesome-icon 
+            icon="fa-solid fa-bell"
+            class="p-2 bg-purple-100 text-purple-400 rounded-full text-sm md:text-base lg:text-lg" 
+            v-else-if="type === 'suggest'" 
+        />
+
+        <div>
+            <span class="flex items-center justify-center gap-2 ">
+                <h2 class="text-xs md:text-sm lg:text-base font-semibold text-sky-900">{{ title }}</h2>
+                <p class="text-xs lg:text-sm text-slate-500 hover:text-green-600 hover:cursor-pointer">{{ subtitle }}</p>
+            </span>
+            <p class="text-xs lg:text-sm text-slate-500">{{ time }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        subtitle: {
+            type: String,
+            required: true
+        },
+        time: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
--- view/src/components/dashboard/SideBar.vue
+++ view/src/components/dashboard/SideBar.vue
@@ -1,58 +1,26 @@
 <template>
-	<nav
-		class="h-full py-8 px-4 flex flex-col gap-6 items-center bg-white basis-1/4 w-full overflow-x-scroll"
-	>
+	<nav class="h-full py-8 px-4 flex flex-col gap-6 items-center bg-white lg:basis-2/5 xl:basis-1/5 w-full overflow-x-scroll">
 		<div class="flex flex-col justify-center items-center gap-2">
-			<img
-				src="../../assets/headHunterLogo.png"
-				alt="avt"
-				class="w-24 h-auto rounded-full"
-			/>
-			<h2 class="text-xs md:text-sm lg:text-base font-bold text-sky-900">
+			<img src="https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/380473750_705477890924386_4811766216330519987_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGFmilbrwjAiwoz971chM1LE5bf8IYqo6oTlt_whiqjqh0Iwl9C4uRPI-ZC7gMedieRRXim92lupTLVVeUE-nxV&_nc_ohc=AuDbYUxThqkAX-QdQEU&_nc_ht=scontent.fhan14-2.fna&oh=03_AdSOy_wvf3J7K-ii0QH99UV-lHuv1HBENNtAqOpozA7T7g&oe=6561863A"
+				alt="avt" class="w-32 h-32 rounded-full" />
+			<h2 class="text-sm md:text-base lg:text-lg font-bold text-sky-900">
 				Nguyen Duc Thien
 			</h2>
-			<p class="text-xs lg:text-sm text-slate-500">Front-end Developer</p>
+			<p class="text-xs md:text-sm lg:text-base text-slate-500">Front-end Developer</p>
 		</div>
 
-		<ul class="w-full">
-			<base-list
-				title="Thông tin chung"
-				icon="fa-solid fa-palette"
-			></base-list>
-			<base-list
-				title="Hồ sơ của tôi"
-				icon="fa-solid fa-user"
-			></base-list>
-			<base-list
-				title="CV của tôi"
-				icon="fa-solid fa-file-pdf"
-			></base-list>
-			<base-list
-				title="Công việc ứng tuyển"
-				icon="fa-solid fa-paper-plane"
-			></base-list>
-			<base-list
-				title="Tin tuyển dụng"
-				icon="fa-solid fa-bell"
-			></base-list>
-			<base-list
-				title="Tin tuyển dụng đã lưu"
-				icon="fa-solid fa-bookmark"
-			></base-list>
-			<base-list
-				title="Công ty đang theo dõi"
-				icon="fa-solid fa-user-check"
-			></base-list>
+		<ul class="w-full flex flex-col gap-1">
+			<base-list title="Thông tin chung" icon="fa-solid fa-palette"></base-list>
+			<base-list title="Hồ sơ của tôi" icon="fa-solid fa-user"></base-list>
+			<base-list title="CV của tôi" icon="fa-solid fa-file-pdf"></base-list>
+			<base-list title="Công việc ứng tuyển" icon="fa-solid fa-paper-plane"></base-list>
+			<base-list title="Tin tuyển dụng" icon="fa-solid fa-bell"></base-list>
+			<base-list title="Tin tuyển dụng đã lưu" icon="fa-solid fa-bookmark"></base-list>
+			<base-list title="Công ty đang theo dõi" icon="fa-solid fa-user-check"></base-list>
 			<base-list title="Tin nhắn" icon="fa-solid fa-message"></base-list>
 			<base-list title="Đổi mật khẩu" icon="fa-solid fa-key"></base-list>
-			<base-list
-				title="Xóa tài khoản"
-				icon="fa-solid fa-trash"
-			></base-list>
-			<base-list
-				title="Đăng xuất"
-				icon="fa-solid fa-right-from-bracket"
-			></base-list>
+			<base-list title="Xóa tài khoản" icon="fa-solid fa-trash"></base-list>
+			<base-list title="Đăng xuất" icon="fa-solid fa-right-from-bracket"></base-list>
 		</ul>
 	</nav>
 </template>
