e10f443977797e587444b5a5781f570f767c32bb
ctpanh <ctpanh03@gmail.com>
2023-12-01 16:09:24 +0700

    fix: company model

--- app/models/company.py
+++ app/models/company.py
@@ -6,5 +6,10 @@ import database
 class CompanyModel(database.Base):
     __tablename__ = "company"
     id = Column(Integer, primary_key=True, index=True)
+    job_quantity = Column(Integer)
     name = Column(String(50), index=True)
+    address = Column(String(300))
+    founded_year = Column(Integer)
+    employee_quantity = Column(Integer)
     description = Column(String(300))
+    contact = Column(String(50))
