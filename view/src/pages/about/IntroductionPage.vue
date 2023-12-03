40df6dfd4ed09e1a6eed398fa2d99f2373a6c4f2
NguyenDucThien <21020407@vnu.edu.vn>
2023-12-04 02:17:54 +0700

    create: introduction page

---
+++ view/src/pages/about/IntroductionPage.vue
@@ -0,0 +1,56 @@
+<template>
+    <section class="intro bg-blue-100 py-12 xl:pt-12 xl:pb-0 overflow-hidden">
+        <div class=" mt-12 container mx-auto h-full " style="color: #0c4a6e;">
+            <div class="flex flex-col xl:flex-row items-center justify-between h-fit">
+                <div class="intro-text xl:w-[48%] text-center xl:text-left">
+                    <div class="flex items-center bg-white py-[10px] px-[20px] w-max gap-x-2 mb-26[px] rounded-full mx-auto xl:mx-0 text-xl">Head Hunter</div>
+                    <div class="h1 mb-6 mt-1">Your Gateway to Talent Excellence!</div>
+                    <p class="mb-[42px] md:max-w-xl">At Head Hunter, we're redefining the way talent meets opportunity. Whether you're on a quest for your dream job or seeking the perfect addition to your team, our platform is the nexus of ambition and expertise.</p>
+                    <button class="flex items-center bg-white py-[10px] px-[25px] w-max gap-x-2 mb-26[px] rounded-full mx-auto xl:mx-0 text-xl">Button</button>
+                </div>
+                
+                
+                <div class="object-contain hidden xl:flex xl:max-w-7xl w-{800} h-{600}">
+                    <img src="../../assets/intro-new.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <br>
+    <br>
+    <section class="stats section">
+        <div class="container mx-auto">
+            <div class="flex flex-col xl:flex-row gap-y-6 justify-between">
+                <div class="stats-item flex flex-1 border-r flex-col items-center">
+                    <div class="text-3xl xl:text-[64px] font-semibold text-blue-500 xl:mb-2">+5000</div>
+                    <div>Candidates Joined</div>
+                </div>
+                <div class="stats-item flex flex-1 border-r flex-col items-center">
+                    <div class="text-3xl xl:text-[64px] font-semibold text-blue-500 xl:mb-2">+50</div>
+                    <div>Companies</div>
+                </div>
+                <div class="stats-item flex flex-1 border-r flex-col items-center">
+                    <div class="text-3xl xl:text-[64px] font-semibold text-blue-500 xl:mb-2">5</div>
+                    <div>Total Branches</div>
+                </div>
+                <div class="stats-item flex flex-1 flex-col items-center">
+                    <div class="text-3xl xl:text-[64px] font-semibold text-blue-500 xl:mb-2">1</div>
+                    <div>Year</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="services">
+        <div class="bg-blue-200 bg-cover bg-no-repeat max-w-[1466px] mx-4 xl:mx-auto rounded-[20px] xl:pt-[70px] px-6 xl:px-0 relative h-[368px] flex items-center xl:items-start -z-10">
+            <div class="container mx-auto">
+                <div class=" flex items-center flex-col xl:flex-row xl:mb-[60px]">
+                    <h2 class="text-3xl text-while flex-1 mb-4 xl:mb-0 text-center xl:text-left">Our best services for your career future</h2>
+                    <p>Dive into a world where success is not just a goal; it's a journey we embark upon together. Join Head Hunter today and witness the power of connections, the synergy of talent, and the realization of professional dreams.</p>
+
+<!-- <p>Your future awaits! 🚀</p> -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="h-[400px]"></div>
+</template>
