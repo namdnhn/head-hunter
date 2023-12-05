b5a8510cbd27f3125a3201809e0515322c5ab7d5
NguyenDucThien <21020407@vnu.edu.vn>
2023-12-05 23:57:44 +0700

    fix: introduction page, create: contact page

--- view/src/pages/about/ContactPage.vue
+++ view/src/pages/about/ContactPage.vue
@@ -1,1 +1,22 @@
-<template></template>
+<template>
+    <section class="intro bg-blue-100 py-12 xl:pt-12 xl:pb-0 overflow-hidden">
+        <div class=" mt-12 container mx-auto h-full " style="color: #0c4a6e;">
+            <div class="flex flex-col xl:flex-row items-center justify-between h-fit">
+                <div class="intro-text xl:w-[48%] text-center xl:text-left">
+                    <div class="h1 mb-6 mt-1">Hãy liên hệ ngay cho chúng tôi nếu bạn cần bất cứ sự hỗ trợ nào</div>
+                    <div>
+                        <ul>Gmail: headhunter@gmail.com</ul>
+                        <ul>Hotline: +(84)12345678</ul>
+                        <ul>Fanpage: https://facebook.com/headhunter</ul>
+                    </div>
+                    
+                </div>
+                
+                
+                <div class="object-contain hidden xl:flex xl:max-w-8xl ">
+                    <img src="../../assets/intro-new.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
